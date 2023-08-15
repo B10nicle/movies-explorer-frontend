e35e424e5ab00cfc06ee984280619c5fbc4df440
Oleg Khilko <khilkoleg@gmail.com>
2023-08-15 22:48:45 +0300

    fix: Authorization.jsx

--- src/components/Authorization/Authorization.jsx
+++ src/components/Authorization/Authorization.jsx
@@ -1,17 +1,17 @@
-import Forms from "../../utils/validation";
-import Logo from "../Logo/Logo";
-import "./Authorization.css";
 import React from "react";
+import "./Authorization.css";
+import Logo from "../Logo/Logo";
+import Forms from "../../utils/validation";
 
 function Authorization(props) {
-    const {values, errors, handleChange, handleSubmit} = Forms(props.onLogin);
+    const { values, errors, handleChange, handleSubmit } = Forms(props.onLogin);
 
     return (
         <section className="authorization">
             <div className="authorization__header">
                 <div className="authorization__header-container">
                     <div className="authorization__logo-container">
-                        <Logo/>
+                        <Logo />
                     </div>
                     <h1 className="authorization__text">{props.title}</h1>
                 </div>
@@ -23,14 +23,14 @@ function Authorization(props) {
                             <p className="authorization__name">Имя</p>
                             <input
                                 className="authorization__input"
+                                placeholder="Имя"
+                                type="text"
+                                id="name"
+                                name="name"
+                                minLength="2"
+                                maxLength="30"
                                 value={values.name || ""}
                                 onChange={handleChange}
-                                placeholder="Имя"
-                                maxLength="30"
-                                minLength="2"
-                                type="text"
-                                name="name"
-                                id="name"
                                 required
                             />
                         </li>
@@ -41,71 +41,66 @@ function Authorization(props) {
                     <li className="authorization__part-form">
                         <p className="authorization__name">E-mail</p>
                         <input
-                            pattern="[A-Za-z0-9._+\-']+@[A-Za-z0-9.\-]+\.[A-Za-z]{2,}$"
                             className="authorization__input"
                             placeholder="E-mail"
-                            maxLength="30"
-                            minLength="2"
-                            name="email"
                             type="email"
                             id="email"
+                            name="email"
+                            minLength="2"
+                            maxLength="30"
+                            pattern="[A-Za-z0-9._+\-']+@[A-Za-z0-9.\-]+\.[A-Za-z]{2,}$"
                             value={values.email || ""}
                             onChange={handleChange}
                             required
                         />
                     </li>
-                    <p className="authorization__validation-text">
-                        {errors.email && "Пример email: user@example.com"}
-                    </p>
+                    <p className="authorization__validation-text">{errors.email && "Введите данные в формате name@hostname.ru"}</p>
                     <li className="authorization__part-form">
                         <p className="authorization__name">Пароль</p>
                         <input
-                            pattern="^(?=\S*[a-z])(?=\S*[A-Z])(?=\S*\d)(?=\S*[^\w\s])\S{8,}$"
                             className="authorization__input"
-                            value={values.password || ""}
-                            onChange={handleChange}
                             placeholder="Пароль"
                             type="password"
-                            name="password"
-                            maxLength="30"
-                            minLength="2"
                             id="password"
+                            name="password"
+                            minLength="2"
+                            maxLength="30"
+                            pattern="^(?=\S*[a-z])(?=\S*[A-Z])(?=\S*\d)(?=\S*[^\w\s])\S{8,}$"
+                            value={values.password || ""}
+                            onChange={handleChange}
                             required
                         />
                     </li>
-                    <p className="authorization__validation-text">
-                        {errors.password && "Пароль должен содержать как минимум 8 символов. Одну букву нижнего " +
-                            "регистра, одну верхнего, одну цифру и хотя бы один спецсимвол"}
-                    </p>
+                    <p className="authorization__validation-text">{errors.password && "Пароль должен содержать одну цифру от 1 до 9, одну строчную букву, одну заглавную букву, один специальный символ, без пробела и должен состоять из 8-16 символов."}</p>
                 </ul>
                 <div className="authorization__confirm">
                     <button
                         className={`${
                             props.authType === "register"
                                 ? "authorization__confirm-button"
-                                : "authorization__confirm-button authorization__confirm-button-login"}
-            ${props.authType === "register"
-                            ? (errors.name ||
-                                errors.email ||
-                                errors.password ||
-                                values.name === undefined ||
-                                values.email === undefined ||
-                                values.password === undefined) &&
-                            "authorization__confirm-button_disable"
-                            : (errors.email ||
-                                errors.password ||
-                                values.email === undefined ||
-                                values.password === undefined) &&
-                            "authorization__confirm-button_disable"
+                                : "authorization__confirm-button authorization__confirm-button-login"
+                        }
+            ${
+                            props.authType === "register"
+                                ? (errors.name ||
+                                    errors.email ||
+                                    errors.password ||
+                                    values.name === undefined ||
+                                    values.email === undefined ||
+                                    values.password === undefined) &&
+                                "authorization__confirm-button_disable"
+                                : ( errors.email ||
+                                    errors.password ||
+                                    values.email === undefined ||
+                                    values.password === undefined) &&
+                                "authorization__confirm-button_disable"
                         }`}
                         type="submit"
                     >
                         {props.button}
                     </button>
                     <div className="authorization__confirm-container">
-                        <p className="authorization__confirm-text">
-                            {props.text}
-                        </p>
+                        <p className="authorization__confirm-text">{props.text}</p>
                         <a className="authorization__confirm-link" href={props.linkRout}>
                             {props.link}
                         </a>
