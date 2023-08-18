3ec3b9fc0fc48fbbbe75bae3ee0c6a6902d330d6
Oleg Khilko <92729800+B10nicle@users.noreply.github.com>
2023-08-18 12:13:31 +0300

    feature: level-3

---
+++ src/components/Movies/SearchError/SearchError.jsx
@@ -0,0 +1,12 @@
+import "./SearchError.css";
+import React from "react";
+
+function SearchError() {
+    return (
+        <p className="search-error_not-found">
+            Ничего не найдено
+        </p>
+    );
+}
+
+export default SearchError;
